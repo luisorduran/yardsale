dce347361c3c64bccfc82e980106b689a5953ab8
Luis Durán <32286433+luisorduran@users.noreply.github.com>
2022-08-31 18:28:55 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const menuEmail = document.querySelector(".navbar-email");
+const desktopMenu = document.querySelector(".desktop-menu");
+const burguerMenu = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const carMenu = document.querySelector(".navbar-shopping-cart");
+const aside = document.querySelector(".product-detail");
+
+menuEmail.addEventListener("click", toggleDesktopMenu);
+
+burguerMenu.addEventListener("click", toggleMobileMenu);
+
+carMenu.addEventListener("click", toggleCarMenu);
+
+function toggleDesktopMenu() {
+  desktopMenu.classList.toggle("inactive");
+}
+
+function toggleMobileMenu() {
+  if (!aside.classList.contains("inactive")) {
+    aside.classList.add("inactive");
+  }
+  mobileMenu.classList.toggle("inactive");
+}
+
+function toggleCarMenu() {
+  if (!mobileMenu.classList.contains("inactive")) {
+    mobileMenu.classList.add("inactive");
+  }
+  aside.classList.toggle("inactive");
+}
